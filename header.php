3cc8e623cfb075a8ad74276bf13a1a64331ee771
Christopher Bishop <domingobishop@gmail.com>
2016-09-11 21:41:57 +0100

    header logo change

--- header.php
+++ header.php
@@ -23,7 +23,9 @@
                 <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="<?php bloginfo('name'); ?>" class="img-responsive">
             </div>
             <div class="col-md-6 text-right">
-                <h3><?php bloginfo('description'); ?></h3>
+                <div class="tagline-logo clearfix">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/logo-tag.jpg" alt="<?php bloginfo('description'); ?>" class="img-responsive pull-right">
+                </div>
                 <p>
                     <a href="https://www.facebook.com/ClareMastersGames/" target="_blank"><img src="<?php bloginfo('stylesheet_directory'); ?>/img/facebook.png"></a>
                     <a href="#" target="_blank"><img src="<?php bloginfo('stylesheet_directory'); ?>/img/twitter.png"></a>
